18491cba36c047855b89010bd01c7d5e33e51e0c
Jkig <DerekLeroux47@gmail.com>
2024-07-14 13:14:13 -0600

    Got some spacing figured out, this is the way i'll start to go

--- binaryTrees/src/App.jsx
+++ binaryTrees/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 
-// let NodeArray = [1,2,3];
+let NodeArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let nodeArrIndex = 0;
+// I'm going to make a node array where each place where there isn't a node, I'll put a null
+// this makes it a bit easier for now, I know its not efficient and doeesn't scale, but this doesn't matter, I'm only allowing small arrays for now
 
 class treeNode {
   constructor(value, left=null, right=null) {
@@ -16,11 +19,13 @@ class treeNode {
 // make functions that convert all traversals to all others, and try to show off how that happens
 // display the nodes
 
-function displaySingleNode(val) {
+function displaySingleNode(val, look=0) {
   // for null ones, have it fade into backgorund
+  // have null, normal, and highlighted
+  // TODO: should have an id for each, for now just using value
   return (
     <>
-      <span className="node">
+      <span className={look === 0 ? "node" : look === 2 ? "highLitNode" : "nullNode"}>
         <p>{val}</p>
       </span>
     </>
@@ -29,9 +34,23 @@ function displaySingleNode(val) {
 
 function displayNodeLayer(maxNodes) {
   // use a global node array and index to display from
+  // make an array here that can put them all in a line
+  let allLayers = []
+
+  for (let i=0;i<maxNodes;i++){
+    allLayers.push(displaySingleNode(maxNodes + i));
+    if (maxNodes === 2 && i === 0){
+      allLayers.push(displaySingleNode(null,1));
+      allLayers.push(displaySingleNode(null,1));
+      allLayers.push(displaySingleNode(null,1));
+    }
+    if (maxNodes === 4 && i != 3){
+      allLayers.push(displaySingleNode(null,1));
+    }
+  }
   return (
     <div className='fullLayer' key={maxNodes}>
-      {displaySingleNode(maxNodes)}
+      {allLayers}
     </div>
   )
 
@@ -39,11 +58,12 @@ function displayNodeLayer(maxNodes) {
 
 function displayNodes() {
   // displays all the nodes
-  let layers = 2;// have this become number of layers
+  let layers = 4;// have this become number of layers
   let allLayers = []
-
-  for (let i=0;i<layers;i++){
-    allLayers.push(displayNodeLayer(i));
+  let j = 1;
+  for (let i=1;i<=layers;i++){
+    allLayers.push(displayNodeLayer(j));
+    j*=2;
   }
   return <div>{allLayers}</div>
 }
@@ -52,12 +72,12 @@ function displayNodes() {
 function App() {
   // select from a few basic trees to try
   // baseBinaryTree
-
+  console.log("Fresh Reload");
   return (
     <>
       <h1>Binary Tres</h1>
-      <h3>Put your tree here [1,2,3]</h3>
-      <h4>Type of Traversal [inorder others?]</h4>
+      <h3>Put your tree here [{NodeArray.join(", ")}]</h3>
+      <h4>Type of Traversal [dfs, bfs, etc...]. Start Traversal</h4>
       {displayNodes()}
     </>
   )
